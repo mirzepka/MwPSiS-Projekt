34d08cb3cfeeb5c200b0a41bcf2e2523b2c272c2
pt <pat@example.com>
2018-12-09 19:21:34 +0100

    update insturkcji

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ PROJ=/home/pat/MwPSiS-projekt
 cd "$PROJ"
 ./ModelDataGenerator/Generator "$1" >> /dev/null
 
-./solve_gmpl model.mod data.dat | tee tmp.log | sed -n '/^y\[/p' 
+./solve_gmpl model.mod data.dat | tee tmp.log | sed -n '/^y\[/p' > res.txt
+
+curl --request POST --data-binary "@res.txt" 127.0.0.1/cgi-bin/upload.sh
 
 rm -f tmp.log
