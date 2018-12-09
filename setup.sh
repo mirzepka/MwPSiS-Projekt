a7eae404438c2f0c6e1a10400cb853e519aed960
Grzegorz <grzegorzgadek8@gmail.com>
2018-12-09 21:50:18 +0100

    naprawa wyswietlania

--- setup.sh
+++ setup.sh
@@ -7,15 +7,15 @@ cd "$PROJ"
 ./ModelDataGenerator/Generator "$1" >> /dev/null
 
 start=$(($(date +%s%N)/1000000)); 
-./solve_gmpl model.mod data.dat | grep FINAL  | sed 's/FINAL_LOG://' | tee res.txt
+./solve_gmpl model.mod data.dat | grep FINAL  | sed 's/FINAL_LOG://' | tr '\n' 'Y' | sed 's/Y/\<br\>/g' | tee res.txt
 end=$(($(date +%s%N)/1000000)); 
 
-echo "Execution time: "$(((end-start)/1000))s | tee -a res.txt
+echo "Execution time: "$(((end-start)/1000))"s<br><br>" | tee -a res.txt
 
 start=$(($(date +%s%N)/1000000)); 
-./Heuristic/heuristic data.dat | grep FINAL  | sed 's/FINAL_LOG://' | tee -a res.txt
+./Heuristic/heuristic data.dat | grep FINAL  | sed 's/FINAL_LOG://' | tr '\n' 'Y' | sed 's/Y/\<br\>/g' | tee -a res.txt
 end=$(($(date +%s%N)/1000000)); 
 
-echo "Execution time: "$(((end-start)/1000))s | tee -a res.txt
+echo "Execution time: "$(((end-start)/1000))"s<br><br>" | tee -a res.txt
 
 curl --request POST --data-binary "@res.txt" 127.0.0.1/cgi-bin/upload.sh
